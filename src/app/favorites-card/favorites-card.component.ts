82033d062d0914cb00e0fbceedd0d856876ad9e4
Matt H <mhh129@gmail.com>
2024-07-14 17:13:39 -0400

    Update favorites-card template to display tv favorites data

--- src/app/favorites-card/favorites-card.component.ts
+++ src/app/favorites-card/favorites-card.component.ts
@@ -1,18 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { TvShowDetails } from '../shared/interfaces/tv-show-details';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FavoritesService } from '../services/favorites/favorites.service';
 
 @Component({
   selector: 'app-favorites-card',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, RouterModule ],
   templateUrl: './favorites-card.component.html',
   styleUrl: './favorites-card.component.css'
 })
 export class FavoritesCardComponent implements OnInit {
-  @Input() data?: TvShowDetails
+  @Input() favoriteData?: TvShowDetails
 
+  protected favoritesService = inject(FavoritesService);
 
   ngOnInit(): void {
-    console.log('--- this.data:', this.data)
+    console.log('--- this.favoriteData:', this.favoriteData)
+  }
+
+  handleToggleFavorites(showId: number): void {
+    
   }
 }
