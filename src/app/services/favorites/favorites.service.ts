26192a5d5d25dcdb8018f05f049369732f45344d
Matt H <mhh129@gmail.com>
2024-07-14 13:04:03 -0400

    Update favorites service to handle toggling favorites using signals

--- src/app/services/favorites/favorites.service.ts
+++ src/app/services/favorites/favorites.service.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable } from '@angular/core';
+import { effect, inject, Injectable, signal, WritableSignal } from '@angular/core';
 import { StorageService } from '../storage/storage.service';
 
 @Injectable({
@@ -7,18 +7,36 @@ import { StorageService } from '../storage/storage.service';
 export class FavoritesService {
   private storage = inject(StorageService<number[]>);
   private storageKey = 'at-favorite-shows';
+  private favoritesSignal: WritableSignal<number[]> = signal(this.storage.getStorage(this.storageKey));
+  favorites = this.favoritesSignal.asReadonly()
 
-  constructor() { }
-
-  toggleFavorite(showId: number): void {
-    let showIds: number[] = this.storage.getStorage(this.storageKey);
-    showIds.includes(showId)
-      ? showIds = showIds.filter(id => id !== showId)
-      : showIds.push(showId, ...showIds);
-    this.storage.setStorage(this.storageKey, showIds);
+  constructor() {
+    effect(() => {
+      console.log('--- effect ---')
+      this.storage.setStorage(this.storageKey, this.favoritesSignal())
+  });
   }
 
-  getFavorites(): number[] {
-    return this.storage.getStorage(this.storageKey);
+  toggleFavorite(showId: number): void {
+    if (this.favoritesSignal().includes(showId)) {
+      // Option 1
+      this.favoritesSignal.set(this.favoritesSignal().filter(x => x !== showId));
+      
+      // Option 2
+      // this.favoritesSignal.update(x => { return this.favoritesSignal().filter(y => y !== showId) });
+    } else {
+      // Option 1
+      const updatedFavorites: number[] = this.favoritesSignal();
+      updatedFavorites.push(showId);
+      this.favoritesSignal.set(updatedFavorites);
+      this.storage.setStorage(this.storageKey, this.favoritesSignal());
+      
+      // Option 2
+      // this.favoritesSignal.update(x => { x.push(showId); return x.slice(0) });
+      
+      // Option 3
+      // this.favoritesSignal().push(showId);
+      // this.storage.setStorage(this.storageKey, this.favoritesSignal());
+    }
   }
 }
