e6b2313865e32434560e238502271f4a65c03e13
Matt H <mhh129@gmail.com>
2024-07-22 19:06:19 -0400

    Update favorites-toggle-image directive to handle toggling favorite icon css highlight class

--- src/app/shared/directives/favorites-toggle-image/favorites-toggle-image.directive.ts
+++ src/app/shared/directives/favorites-toggle-image/favorites-toggle-image.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, inject } from '@angular/core';
+import { Directive, HostBinding, HostListener, inject, Input } from '@angular/core';
 import { FavoritesService } from 'src/app/services/favorites/favorites.service';
 
 @Directive({
@@ -6,13 +6,20 @@ import { FavoritesService } from 'src/app/services/favorites/favorites.service';
   standalone: true
 })
 export class FavoritesToggleImageDirective {
-
   protected favoritesService = inject(FavoritesService);
+
+  @Input() showId!: number;
+  
+  @HostBinding('class.highlight') 
+  get isFavorite() { 
+    return this.favoritesService.favorites().includes(this.showId)
+  }
+
+  @HostListener('click') onClick(event: Event) : void {
+    if (this.showId) this.favoritesService.toggleFavorite(this.showId)
+  }
+
   
   constructor() { }
-  
-  handleToggleFavorites(id: number): void {
-    this.favoritesService.toggleFavorite(id);
-  }
 
 }
--- src/app/tv-show-table/tv-show-table.component.ts
+++ src/app/tv-show-table/tv-show-table.component.ts
@@ -4,21 +4,23 @@ import { TvShows } from '../shared/interfaces/tv-shows';
 import { Observable } from 'rxjs';
 import { FavoritesService } from '../services/favorites/favorites.service';
 import { RouterModule } from '@angular/router';
+import { FavoritesToggleImageDirective } from '../shared/directives/favorites-toggle-image/favorites-toggle-image.directive';
 
 @Component({
   selector: 'app-tv-show-table',
   standalone: true,
-  imports: [ CommonModule, RouterModule ],
+  imports: [ FavoritesToggleImageDirective, CommonModule, RouterModule ],
   templateUrl: './tv-show-table.component.html',
   styleUrls: ['./tv-show-table.component.css']
 })
-export class TvShowTableComponent {
+export class TvShowTableComponent extends FavoritesToggleImageDirective {
+// export class TvShowTableComponent {
   @Input() tvShowsData$?: Observable<TvShows> | null;
 
-  protected favoritesService = inject(FavoritesService);
+  // protected favoritesService = inject(FavoritesService);
   
-  handleToggleFavorites(id: number): void {
-    this.favoritesService.toggleFavorite(id);
-  }
+  // handleToggleFavorites(id: number): void {
+  //   this.favoritesService.toggleFavorite(id);
+  // }
 
 }
